ac9b977efe07b25ab2e373ea10dc09babaacd396
jim <wowdacom@gmail.com>
2021-03-03 18:30:37 +0800

    test template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("ker ker");
+import "./style.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+  let p = document.createElement("p");
+  p.textContent = "Hello Wepack!";
+
+  let sectionCotent = document.querySelector("#color");
+  console.log(sectionCotent);
+  sectionCotent.value = "Hello My Webpack Template!";
+});
